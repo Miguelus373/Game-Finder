0edfdee8079586fa03fcedb9f418573380140c7f
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-03-20 14:28:47 -0400

    Add basic files & project structure

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import '../assets/App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://gamefinderapi.herokuapp.com/games"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Games in Backend API
+        </a>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
