dba025c526bae98d5f68107964648bfd9355294c
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-03-24 12:54:23 -0400

    Add styles to Menu component

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -5,6 +5,8 @@ import { Link, useHistory } from 'react-router-dom';
 import { setUser } from '../actions';
 import * as API from '../helpers/api';
 import { loggedIn, currentUserId, userToken } from '../helpers/session';
+import styles from '../assets/Menu.module.css';
+import profileIcon from '../assets/images/profile_icon.png';
 
 const Menu = () => {
   const cookies = new Cookies();
@@ -28,32 +30,44 @@ const Menu = () => {
     history.push('/');
   };
 
-  const user = useSelector(state => state.user.info);
+  const user = useSelector(state => state.user);
 
-  if (user) {
+  if (user.info) {
+    const { info: { username }, favs } = user;
     return (
-      <>
-        <p>
-          Welcome
-          {' '}
-          {user.username}
-        </p>
-        <Link to="/games">
+      <div className={styles.menu}>
+        <img src={profileIcon} alt="Profile Icon" className={styles.icon} />
+        <h3 className={styles.username}>
+          @
+          {username}
+        </h3>
+        <Link to="/games" className={styles.link}>
           Games
         </Link>
-        <br />
-        <Link to="/favourites">
+        <Link to="/favourites" className={styles.link}>
           Favourites
+          {' '}
+          <span className={styles.counter}>{favs.length}</span>
         </Link>
-        <hr />
-        <button type="button" onClick={logout}>
+        <p className={styles.link}>
+          Messages
+          <span className={styles.counter}>2</span>
+        </p>
+        <p className={styles.link}>Friends</p>
+        <hr className={styles.line} />
+        <p className={styles.link}>Help</p>
+        <button type="button" onClick={logout} className={styles.logout}>
           Logout
         </button>
-      </>
+      </div>
     );
   }
 
-  return (<h2>Loading</h2>);
+  return (
+    <div className={styles.loading}>
+      <h2>Logging In...</h2>
+    </div>
+  );
 };
 
 export default Menu;
